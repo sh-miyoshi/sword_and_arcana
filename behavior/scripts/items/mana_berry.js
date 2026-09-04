14df6b6aefb3ae9e38d2fb1cafe54e76d2b987ea
s.miyoshi <s.miyoshi1991@gmail.com>
2026-09-04 20:57:56 +0900

    魔力の実のドロップ処理を追加し、葉っぱを破壊した際に10%の確率でアイテムを生成する機能を実装

--- behavior/scripts/items/mana_berry.js
+++ behavior/scripts/items/mana_berry.js
@@ -1,7 +1,8 @@
-import { system } from '@minecraft/server'
+import { system, world, ItemStack } from '@minecraft/server'
 
 import { MAX_MANA, setMana } from '../player/mana.js'
 
+// 魔力の実を使用した時の処理
 system.beforeEvents.startup.subscribe(event => {
   event.itemComponentRegistry.registerCustomComponent('my:restore_mana', {
     onConsume (event) {
@@ -13,4 +14,34 @@ system.beforeEvents.startup.subscribe(event => {
       })
     }
   })
+})
+
+// 手に入れるときの処理
+const LEAVES = new Set([
+  'minecraft:oak_leaves',
+  'minecraft:spruce_leaves',
+  'minecraft:birch_leaves',
+  'minecraft:jungle_leaves',
+  'minecraft:acacia_leaves',
+  'minecraft:dark_oak_leaves',
+  'minecraft:mangrove_leaves',
+  'minecraft:cherry_leaves'
+])
+
+world.afterEvents.playerBreakBlock.subscribe(event => {
+  const blockId = event.brokenBlockPermutation.type.id
+
+  // 葉っぱ以外なら何もしない
+  if (!LEAVES.has(blockId)) {
+    return
+  }
+
+  // 10%の確率でドロップ
+  if (Math.random() >= 0.1) {
+    return
+  }
+
+  const manaBerry = new ItemStack('my:mana_berry', 1)
+
+  event.player.dimension.spawnItem(manaBerry, event.block.location)
 })
